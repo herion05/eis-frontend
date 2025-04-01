f24e6fad06424cc9885af38d08dad6141d38321a
herion05 <herihalilaj1@gmail.com>
2025-04-02 00:19:28 +0200

    Fixes for login form component and small fix for login server action

--- app/login/LoginForm.js
+++ app/login/LoginForm.js
@@ -2,6 +2,7 @@
 
 import { login } from "@/lib/auth";
 import { useState, useActionState } from "react";
+import { ChevronDown } from "lucide-react";
 
 export default function LoginForm() {
   const [isOpened, setIsOpened] = useState(false);
@@ -9,13 +10,16 @@ export default function LoginForm() {
 
   return (
     <>
-      <p onClick={() => setIsOpened(!isOpened)} className="font-bold flex items-center">
-        Or Login with <span>&#8964;</span>
+      <p
+        onClick={() => setIsOpened(!isOpened)}
+        className="font-bold flex items-center gap-1 w-fit pr-0.5 cursor-default">
+        <span className="text-[14px] md:text-[15px] lg:text-[17px]">Or Login with</span>
+        <ChevronDown className={`transition-transform duration-200 ${isOpened ? "rotate-180" : "rotate-0"}`} />
       </p>
-      <div className={`grid transition-all duration-300 ${isOpened ? "grid-rows-[1fr]" : "grid-rows-[0fr]"}`}>
-        <form action={formAction} className="pt-4 flex flex-col gap-3 overflow-hidden">
+      <div className={`grid transition-all duration-200 ${isOpened ? "grid-rows-[1fr]" : "grid-rows-[0fr]"}`}>
+        <form action={formAction} className="pt-4 px-1 flex flex-col gap-3 overflow-hidden">
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
+            <label htmlFor="email" className="block text-[13px] md:text-sm font-medium text-gray-700 mb-1">
               Email
             </label>
             <input
@@ -23,14 +27,14 @@ export default function LoginForm() {
               type="email"
               name="email"
               required
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md text-[14px] md:text-[15px] lg:text-[17px] leading-none focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="your@email.com"
               disabled={isPending}
             />
           </div>
 
           <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
+            <label htmlFor="password" className="block text-[13px] md:text-sm font-medium text-gray-700 mb-1">
               Password
             </label>
             <input
@@ -38,7 +42,7 @@ export default function LoginForm() {
               type="password"
               name="password"
               required
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md text-[14px] md:text-[15px] lg:text-[17px] leading-none focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="••••••••"
               disabled={isPending}
             />
@@ -71,12 +75,13 @@ export default function LoginForm() {
               {isPending ? "Logging in..." : "Log In"}
             </button>
           </div>
+          {stateMessage.error && (
+            <div className="mt-4 px-3 py-2.5 bg-red-100 border border-red-400 text-red-700 rounded">
+              {stateMessage.message}
+            </div>
+          )}
         </form>
       </div>
-
-      {stateMessage.error && (
-        <div className="mb-4 p-3 bg-red-100 border border-red-400 text-red-700 rounded">{stateMessage.message}</div>
-      )}
     </>
   );
 }
--- app/login/page.js
+++ app/login/page.js
@@ -4,9 +4,9 @@ import EisLogo from "@/public/eis_logo.svg";
 
 export default function LoginPage() {
   return (
-    <section className="pt-24 max-w-4xl mx-auto flex items-center flex-col gap-6 text-slate-700">
+    <section className="pt-20 max-w-4xl mx-auto flex items-center flex-col gap-6 text-slate-700">
       <Image src={EisLogo} alt="Eis Logo" />
-      <div className="bg-white p-8 md:p-10 md:pb-9 rounded-xl">
+      <div className="bg-white max-w-md p-8 md:p-10 md:pb-9 rounded-xl">
         <div className="flex flex-col items-center mb-5 pb-3 border-b border-gray-600/50">
           <p className="text-lg lg:text-xl font-bold text-center text-slate-800 mb-6 leading-none">
             Login to your account
