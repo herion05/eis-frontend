19260086d594bee7e3e1e43da183abff3a130fb3
herion05 <herihalilaj1@gmail.com>
2025-04-04 19:17:15 +0200

    Small changes to the dashboard

--- app/components/DashboardCard.jsx
+++ app/components/DashboardCard.jsx
@@ -1,17 +1,16 @@
-import React from "react";
 import Link from "next/link";
-import { ArrowRight } from "lucide-react";
+import { CircleArrowRight } from "lucide-react";
 
-function DashboardCard({ title, Icon, bgColor, href }) {
+function DashboardCard({ title, subtitle, Icon, bgColor, href }) {
   return (
     <Link href={href} className={`flex flex-col justify-between p-4 h-44 shadow-md text-white rounded-lg ${bgColor}`}>
       <div className="flex justify-between items-center mb-auto">
         <Icon size={48} className="text-white" />
         <h2 className="text-2xl font-bold text-right flex-1">{title}</h2>
       </div>
-      <div className="mt-auto border-t border-white w-full pt-4 flex justify-between items-center text-base">
+      <div className="mt-auto pt-4 flex justify-between items-center text-base">
         <span>VIEW MORE</span>
-        <ArrowRight size={18} className="text-white stroke-2" />
+        <CircleArrowRight size={18} className="text-white stroke-2" />
       </div>
     </Link>
   );
--- app/lecturer-dashboard/page.js
+++ app/lecturer-dashboard/page.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "@/app/components/Header";
 import Sidebar from "@/app/components/Sidebar";
 import DashboardCard from "@/app/components/DashboardCard";
@@ -9,32 +8,40 @@ import { getUser } from "@/lib/auth";
 
 async function LecturerDashboard() {
   const user = await getUser();
-  if (!user) redirect("/login");
+  // if (!user) redirect("/login");
 
   const dashboardItems = [
     { title: "My Profile", icon: User, bgColor: "bg-blue-400", href: "/profile" },
-    { title: "My Courses", icon: BookOpen, bgColor: "bg-green-600", href: "/courses" },
-    { title: "Student List", icon: Users, bgColor: "bg-blue-400", href: "/students" },
+    { title: "My Courses", subtitle: "List", icon: BookOpen, bgColor: "bg-green-600", href: "/courses" },
+    { title: "Student", subtitle: "List", icon: Users, bgColor: "bg-blue-400", href: "/students" },
     { title: "Advisor", icon: UserCheck, bgColor: "bg-yellow-600", href: "/advisor" },
-    { title: "Admission Undergraduate", icon: Users, bgColor: "bg-teal-500", href: "/admission-undergrad" },
-    { title: "Admission Graduate", icon: Users, bgColor: "bg-teal-500", href: "/admission-grad" },
-    { title: "Survey Results", icon: ClipboardCheck, bgColor: "bg-green-600", href: "/survey" },
+    {
+      title: "Admission",
+      subtitle: "Undergraduate",
+      icon: Users,
+      bgColor: "bg-teal-500",
+      href: "/admission-undergrad",
+    },
+    { title: "Admission", subtitle: "Graduate", icon: Users, bgColor: "bg-teal-500", href: "/admission-grad" },
+    { title: "Survey", subtitle: "My Results", icon: ClipboardCheck, bgColor: "bg-green-600", href: "/survey" },
     { title: "Reports", icon: BarChart, bgColor: "bg-green-600", href: "/reports" },
-    { title: "Staff List", icon: Users, bgColor: "bg-purple-500", href: "/staff" },
-    { title: "Job Follow Up", icon: Briefcase, bgColor: "bg-red-500", href: "/job" },
-    { title: "Staff Application Forms", icon: FilePlus, bgColor: "bg-purple-500", href: "/forms" },
+    { title: "Staff", subtitle: "List", icon: Users, bgColor: "bg-purple-500", href: "/staff" },
+    { title: "Job", subtitle: "Follow Up", icon: Briefcase, bgColor: "bg-red-500", href: "/job" },
+    { title: "Form", subtitle: "Staff Application Forms", icon: FilePlus, bgColor: "bg-purple-500", href: "/forms" },
   ];
 
   return (
     <div className="flex-1 flex flex-col">
       <Header />
-      <div className="flex min-h-screen bg-gray-100">
+      <div className="flex max-md:flex-col min-h-screen">
         <Sidebar />
-        <main className="p-6">
-          <h1 className="text-4xl text-gray-900 font-light mb-6">
-            Welcome, <span className="text-gray-900">Name LastName</span>
-          </h1>
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <main className="px-8 py-6 grow">
+          <div className="container mx-auto">
+            <h1 className="text-4xl text-gray-900 font-light mb-6">
+              Welcome, <span className="text-gray-900">Name LastName</span>
+            </h1>
+          </div>
+          <div className="grid container mx-auto grid-cols-2 lg:grid-cols-4 gap-4">
             {dashboardItems.map((item, idx) => (
               <DashboardCard key={idx} title={item.title} Icon={item.icon} bgColor={item.bgColor} href={item.href} />
             ))}
